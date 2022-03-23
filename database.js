aa73385e08bdea4c2d14ddfa9ea4255dfa35b9a6
KingNoFu <101841199+KingNoFu@users.noreply.github.com>
2022-03-23 17:10:33 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,14 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bcee6fe3dfa399",
+	password: "3bb62112",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_e348eec0e7f6507",
 	secret_key: "aruba,key,install,38y495.erd4"
 };
 exports.config = config;
+
+/*user = root
+  passwor = ""
+  port= "3306"
+  host= localhost */
